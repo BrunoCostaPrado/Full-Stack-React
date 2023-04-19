d3e006c71ffd60bef6594b4a68bfd44e48b984ed
BrunoCostaPrado <202203973541@alunos.estacio.br>
2023-04-19 20:29:03 -0300

    add auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,18 @@ import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import Head from "next/head";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ClerkProvider {...pageProps}>
+      <Head>
+        
+      </Head>
+      <Component {...pageProps} />;
+    </ClerkProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
